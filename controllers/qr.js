c08cdffdb8ea524f4013c94f712fe4ef47ab8c48
Kevin Chiu <chiukk1@gmail.com>
2023-04-19 12:32:14 -0400

    QR issue

--- controllers/qr.js
+++ controllers/qr.js
@@ -3,7 +3,9 @@ const QRCode = require('qrcode');
 const getAction = async (req, res) => {
   try {
     const id = req.params.id;
+    console.log(id)
     const code = await QRCode.toDataURL(`http://${req.headers.host}/qr/${id}`);
+    res.status(200).json({ code });
     return { code };
   } catch (error) {
     res.status(500).json({message: error.message});
